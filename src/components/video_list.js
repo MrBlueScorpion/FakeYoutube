5fdf0ec14d3ed688f3d735be4f4cc1f72f6a2f86
Lixing <zlxjackie@hotmail.com>
2016-04-07 23:12:42 +1000

    add video list and video item compoments

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,3 +1,16 @@
-/**
- * Created by Lixing on 7/04/2016.
- */
+import React from 'react';
+import VideoListItem from './video_list_item';
+
+const VideoList = (props) => {
+  const videoItems = props.videos.map((video) => {
+    return <VideoListItem key={video.etag} video={video} />
+  });
+  
+  return (
+    <ul className="col-md-4 list-group">
+      {videoItems}
+    </ul>
+  );
+};
+
+export default VideoList;
--- src/index.js
+++ src/index.js
@@ -1,22 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 
-import SearchBar from './components/search_bar.js';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyC5e0rOjeh-KK3PgkjXxKxDvVF9r-lanVI';
 
-YTSearch({key: API_KEY, term: 'surfboards'}, function (data) {
-  console.log(data);
-});
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-const App = () => {
-  "use strict";
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  )  
-};
+    this.state = { videos: []};
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.videos[0]} />
+        <VideoList videos={this.state.videos} />
+      </div>
+    )
+  };
+}
 
 ReactDOM.render(<App />, document.querySelector('.container'));
